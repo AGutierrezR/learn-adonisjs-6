034c999fbd5e776557b67703501ba9129ce5fbe7
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-07-25 15:40:13 +0200

    Defining A Structure for our Movie using Models: Part 2
    Se han creado dos metodos en el modelo Movie, estos modelos hacen uso del servicio (de esta manera el Controller no usa el servicio, sino que lo usa el mismo Modelo

--- app/controllers/movies_controller.ts
+++ app/controllers/movies_controller.ts
@@ -1,36 +1,15 @@
 import Movie from '#models/movie'
-import MovieService from '#services/movie_service'
 import type { HttpContext } from '@adonisjs/core/http'
-import { toHtml } from '@dimerapp/markdown/utils'
 
 export default class MoviesController {
   async index({ view }: HttpContext) {
-    const slugs = await MovieService.getSlugs()
-    const movies: Movie[] = []
-
-    for (const slug of slugs) {
-      const md = await MovieService.read(slug)
-      const movie = new Movie()
-
-      movie.title = md.frontmatter.title
-      movie.summary = md.frontmatter.summary
-      movie.slug = slug
-
-      movies.push(movie)
-    }
+    const movies = await Movie.all()
 
     return view.render('pages/home', { movies })
   }
 
   async show({ view, params }: HttpContext) {
-    const md = await MovieService.read(params.slug)
-    const movie = new Movie()
-
-    movie.title = md.frontmatter.title
-    movie.summary = md.frontmatter.summary
-    movie.slug = params.slug
-    movie.abstract = toHtml(md).contents
-
+    const movie = await Movie.find(params.slug)
     return view.render('pages/movies/show', { movie })
   }
 }
